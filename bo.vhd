b916fa6a17bf925828d2547ea39e2ad8ae748e5a
JeanJuba <jeanwsj96@hotmail.com>
2018-05-22 21:53:59 -0300

    Update bo.vhdl with new MUX and renamed ALU.

--- bo.vhd
+++ bo.vhd
@@ -30,32 +30,32 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity bo is
-    Port ( clk, reset, set_sum_sub, set_counter : in STD_LOGIC;
-			  option_sum_sub, option_counter : in  STD_LOGIC_VECTOR(1 downto 0); 
-           acum : in  STD_LOGIC_VECTOR(7 downto 0); --o valor atual
-			  val: in STD_LOGIC_VECTOR(7 downto 0);   --o valor a ser retirado de 'acum' a cada ciclo
-			  acum_sum_sub : BUFFER STD_LOGIC_VECTOR(7 downto 0);
-			  acum_counter : BUFFER STD_LOGIC_VECTOR(7 downto 0);
+    Port ( clk, reset, set_a, set_b, control_sul: in STD_LOGIC;
+			  option_ula_a, option_ula_b : in  STD_LOGIC_VECTOR(1 downto 0); 
+			  acum : in  STD_LOGIC_VECTOR(7 downto 0); --o valor atual
+			  valor_mem: in STD_LOGIC_VECTOR(7 downto 0);   --o valor a ser retirado de 'acum' a cada ciclo
+			  buffer_a_ula : BUFFER STD_LOGIC_VECTOR(7 downto 0);
+			  buffer_b_ula : BUFFER STD_LOGIC_VECTOR(7 downto 0);
 			  menor: out STD_LOGIC
 			  );
 end bo;
 
 architecture Behavioral of bo is
 		
-	--signal temp_mux, temp_sum_sub, temp_counter, temp_sub : STD_LOGIC_VECTOR(7 downto 0);
+	--signal temp_mux, signal_ula_a, signal_ula_b, temp_sub : STD_LOGIC_VECTOR(7 downto 0);
 
 	component reg 
     Port ( clk : in  STD_LOGIC;   --clock
            reset : in  STD_LOGIC; --reset o valor do buffer
            set : in  STD_LOGIC;   --seta o valor do buffer com o valor do mux
-			  input : in STD_LOGIC_VECTOR(7 downto 0 ); --a entrada que será somada ou subtraída ao valor
+		   input : in STD_LOGIC_VECTOR(7 downto 0 ); --a entrada que será somada ou subtraída ao valor
            valor : BUFFER  STD_LOGIC_VECTOR(7 downto 0)); --o valor armazenado
 	end component;
 	
 	component mux 
     Port ( a : in  STD_LOGIC_VECTOR(7 downto 0);
            b : in  STD_LOGIC_VECTOR(7 downto 0);
-			  c : in  STD_LOGIC_VECTOR(7 downto 0);
+		   c : in  STD_LOGIC_VECTOR(7 downto 0);
            opt : in  STD_LOGIC_VECTOR(1 downto 0); -- 0 soma 1 subtracao 
            s : out  STD_LOGIC_VECTOR(7 downto 0));
 end component;
@@ -66,17 +66,20 @@ end component;
            s : out  STD_LOGIC);
 end component;
 	
-	signal temp_sum_sub, temp_counter : STD_LOGIC_VECTOR(7 downto 0);
+	signal signal_ula_a, signal_ula_b, mux_out : STD_LOGIC_VECTOR(7 downto 0);
 
 begin
 	
-	reg_sum_sub	: reg port map (clk, reset, set_sum_sub, temp_sum_sub, acum_sum_sub);  --Registrador responsável pela soma e subtração
-	reg_counter : reg port map (clk, reset, set_counter, temp_counter, acum_counter);  --Registrador que conta os itens, ele recebe set '1' até que a memória esteja vazia
+	registrador_a : reg port map (clk, reset, set_a, signal_ula_a, buffer_a_ula);  --Registrador responsável pela soma e subtração
+	registrador_b : reg port map (clk, reset, set_b, signal_ula_b, buffer_b_ula);  --Registrador que conta os itens, ele recebe set '1' até que a memória esteja vazia
+	--falta registrador que conta número de divisões
 	
-	mux_sum_sub : mux port map (acum_sum_sub, val, acum_counter, option_sum_sub, temp_sum_sub); --deverá fazer acum + valor mem ou acum - counter
-	mux_counter : mux port map (acum_counter, "00000001", "00000000", option_counter, temp_counter); --só soma o counter acumulado com +1
+	mux_mem_b : mux port map (valor_mem, buffer_b_ula, control_sum, mux_out);
 	
-	check_men : check_menor port map (temp_sum_sub, val, menor); --checa se val é maior que o acumulado se for então menor '1'
+	alu_a : alu port map (buffer_a_ula,    mux_out, option_ula_a, signal_ula_a); --deverá fazer acum + valor mem ou acum - counter
+	alu_b : alu port map (buffer_b_ula, "00000001", option_ula_b, signal_ula_b); --só soma o counter signal_ula_a com +1
+	
+	compare : comparator port map (signal_ula_a, signal_ula_b, menor); --checa se valor_mem é maior que o signal_ula_a se for então menor '1'
 
 end Behavioral;
 
